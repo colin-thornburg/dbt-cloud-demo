823fe5c207c3ad063304c52140ca9cfa555c3032
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2022-07-13 19:15:07 +0000

    changed column name

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ select
     state as status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount_dollars,
+    amount / 100 as amount_USD,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
