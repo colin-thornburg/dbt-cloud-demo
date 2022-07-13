bbc0c29f16e8aa0ba574c5ef1cbd6d71712ec961
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2022-07-13 17:45:54 +0000

    change stg_payment to amt dollars

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ select
     state as status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    amount / 100 as amount_dollars,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
