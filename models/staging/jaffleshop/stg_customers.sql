1adc6c1fcc0557950219e8f63212d4256de4e8f2
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2022-09-14 14:25:56 +0000

    changed last nm

--- models/staging/jaffleshop/stg_customers.sql
+++ models/staging/jaffleshop/stg_customers.sql
@@ -3,7 +3,7 @@ with customers as (
     select 
         id as customer_id,
         first_name,
-        last_name as last_nm
+        last_name
 
     from {{ source('jaffle_shop', 'customers') }}
 )
